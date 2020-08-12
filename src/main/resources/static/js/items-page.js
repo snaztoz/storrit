9ac4a73ac83ee1aedbebe2386b44e5af6f31d759
snaztoz <hafidhmn@gmail.com>
2020-08-12 13:23:39 +0700

    Add create new item page.

--- src/main/resources/static/js/items-page.js
+++ src/main/resources/static/js/items-page.js
@@ -1,11 +1,51 @@
-$('#site-items-page').carousel();
+/* ===================== Memuat Data Barang =====================
+ *
+ * Untuk mengambil data-data barang dari server.
+ */
 
+var ITEMS_API_URL = '/items';
+
+/* ====================== Pergantian Page =======================
+ *
+ * Perpindahan page di bagian Items menggunakan teknik carousel
+ * dari Bootstrap untuk mengatur pergerakan slide-slidenya.
+ */
+
+/**
+ * Untuk debug dan development, pasang nilai sesuai dengan nomor
+ * page terkait.
+ * Default: 0
+ */
+let ITEMS_INIT_PAGE = 0;
+
+/** Menginisialisasikan ke page pertama ketika aplikasi di-load. */
+$('#site-items-page').carousel(ITEMS_INIT_PAGE);
+
+/**
+ * Membuat page bergeser ke page detail ketika salah satu baris
+ * dari table diklik.
+ * Pada halaman detail tersebut dimuat data-data mengenai barang
+ * barang terkait.
+ */
 $('#site-table-content tr').each(function () {
 	$(this).click(function() {
-		$('#site-items-page').carousel('next');
+		$('#site-items-page').carousel(1);
 	})
 });
 
-$('#site-edit-page-back-btn').click(function() {
-	$('#site-items-page').carousel('prev');
+/**
+ * Pergerakan dari halaman tabel, menuju ke halaman tambah
+ * barang baru.
+ */
+$('#site-table-page-create-item-btn').click(function() {
+	// memutar langsung ke item carousel ketiga
+	$('#site-items-page').carousel(2);
+});
+
+/**
+ * Untuk pergerakan dari halaman detail atau create, kembali ke
+ * halaman tabel.
+ */
+$('.site-table-page-back-btn').click(function() {
+	$('#site-items-page').carousel(0);
 });
