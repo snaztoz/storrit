b8068c89268943edd612185112d8f75ae7f91c61
snaztoz <hafidhmn@gmail.com>
2020-08-14 21:57:05 +0700

    Change items-page.js script structure.
    Make migrations in the script file from using only jQuery, to also
    using Backbone JS.

--- src/main/resources/static/js/items-page.js
+++ src/main/resources/static/js/items-page.js
@@ -3,49 +3,158 @@
  * Untuk mengambil data-data barang dari server.
  */
 
-var ITEMS_API_URL = '/items';
-
-/* ====================== Pergantian Page =======================
+/**
+ * Namespace untuk script page-page items. Keyword var digunakan agar
+ * script lain dapat mengakses object ini.
  *
- * Perpindahan page di bagian Items menggunakan teknik carousel
- * dari Bootstrap untuk mengatur pergerakan slide-slidenya.
+ * (contoh, lihat script sections-navigation.js).
  */
+var item = {};
+
+/* URL dari API yang digunakan untuk mengambil data barang. */
+item.API_URL = '/items';
 
 /**
- * Untuk debug dan development, pasang nilai sesuai dengan nomor
- * page terkait.
- * Default: 0
+ * Page-page yang ada pada bagian item. Angka-angka berikut merupakan
+ * representasi dari urutan page pada frame carousel.
+ *
+ * Secara default yang akan ditampilkan ketika aplikasi pertama kali
+ * dimuat adalah INDEX_PAGE.
  */
-let ITEMS_INIT_PAGE = 0;
-
-/** Menginisialisasikan ke page pertama ketika aplikasi di-load. */
-$('#site-items-page').carousel(ITEMS_INIT_PAGE);
+item.INDEX_PAGE = 0;
+item.UPDATE_PAGE = 1;
+item.CREATE_PAGE = 2;
 
 /**
- * Membuat page bergeser ke page detail ketika salah satu baris
- * dari table diklik.
- * Pada halaman detail tersebut dimuat data-data mengenai barang
- * barang terkait.
+ * Model dasar untuk tiap Item.
  */
-$('#site-table-content tr').each(function () {
-	$(this).click(function() {
-		$('#site-items-page').carousel(1);
-	})
+item.Model = Backbone.Model.extend({
+
+	defaults: {
+		name: '',
+		code: '',
+		amount: 0,
+		amountUnit: ''
+	}
+
 });
 
 /**
- * Pergerakan dari halaman tabel, menuju ke halaman tambah
- * barang baru.
+ * Kumpulan dari banyak model Item.
  */
-$('#site-table-page-create-item-btn').click(function() {
-	// memutar langsung ke item carousel ketiga
-	$('#site-items-page').carousel(2);
+item.Collection = Backbone.Collection.extend({
+
+	model: item.Model,
+
+	/**
+	 * Data terbaru yang didapatkan dari respon permintaan Ajax yang
+	 * dikirim oleh collection ini.
+	 */
+	responseData: {},
+
+	url: item.API_URL,
+
+	parse: function(data) {
+		this.responseData = data;
+		return data._embedded.items;
+	},
+
 });
 
 /**
- * Untuk pergerakan dari halaman detail atau create, kembali ke
- * halaman tabel.
+ * View yang dibuat khusus untuk menghandle event-event terkait
+ * collection dari items.
  */
-$('.site-table-page-back-btn').click(function() {
-	$('#site-items-page').carousel(0);
+item.View = Backbone.View.extend({
+
+	el: '#site-items-page',
+
+	events: {
+		'click .item-table-row': 	'showUpdatePage',
+		'click #item-create-btn': 	'showCreatePage',
+
+		'click .item-table-page-back-btn': 'showIndexPage',
+	},
+
+	/**
+	 * Main template untuk view ini, yakni template utama yang memuat
+	 * semua page untuk section item.
+	 */
+	template: _.template($('#item-page-template').html()),
+
+	/** Template untuk baris yang menampilkan tulisan 'tidak ada data'. */
+	templateEmptyTable: _.template($('#item-page-table-no-data-template').html()),
+
+	/** Template spinner ketika data pada tabel masih dimuat. */
+	templateLoadingTable: _.template($('#item-page-table-loading-template').html()),
+
+	/** Template untuk tiap baris dari tabel. */
+	templateRowTable: _.template($('#item-page-table-row-template').html()),
+
+	/**
+	 * Render item section sebelum event-event terkait section ini dikaitkan
+	 * dengan para handlernya.
+	 */
+	preinitialize: function() {
+		this.render();
+	},
+
+	/**
+	 * Akan dijalankan ketika instance dari view ini pertama kali
+	 * dibuat.
+	 */
+	initialize: function() {
+		this.listenTo(this.collection, 'request', this.renderLoading);
+		this.listenTo(this.collection, 'change sync', this.renderRows);
+		this.collection.fetch();
+
+		this.$el.carousel(item.INDEX_PAGE); // aktifkan carousel
+	},
+
+	/**
+	 * Fungsi render utama yang akan memuat item section (selain index,
+	 * termasuk juga page update dan juga create).
+	 */
+	render: function() {
+		$('#main-section').html(this.template);
+		return this;
+	},
+
+	/** Menampilkan ikon loading. */
+	renderLoading: function() {
+		this.$('#item-table-content').html(this.templateLoadingTable);
+		return this;
+	},
+
+	/** Menampilkan tiap data yang ada di dalam collection. */
+	renderRows: function() {
+		if (this.collection.length === 0) {
+			this.$('#item-table-content').html(this.templateEmptyTable);
+			return this;
+		}
+
+		this.$('#item-table-content').html(this.template);
+		return this;
+	},
+
+	/** Berganti ke page create new item. */
+	showCreatePage: function() {
+		this.$el.carousel(item.CREATE_PAGE);
+	},
+
+	/** Berganti ke page index dari item. */
+	showIndexPage: function() {
+		this.$el.carousel(item.INDEX_PAGE);
+	},
+
+	/** Berganti ke page update terkait item yang diklik. */
+	showUpdatePage: function() {
+		this.$el.carousel(item.UPDATE_PAGE);
+	},
+
 });
+
+// Collection ini harus dapat diakses secara global agar ketika instance
+// dari item.View dibuat di dalam script lain (misal sections-navigation.js),
+// view tersebut dapat dipasangkan dengan collection ini.
+var itemCollection = new item.Collection();
